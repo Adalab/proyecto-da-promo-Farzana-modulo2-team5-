d6f5029f58beb8100ca407d6dc31dc9eddba1584
Karvala <saragsandin@gmail.com>
2023-11-18 13:16:44 +0100

    queries para responder preguntas

--- sql/cambios_datatype.sql
+++ sql/cambios_datatype.sql
@@ -7,6 +7,12 @@
  UPDATE puntuaciones
      SET puntos_rotten='' 
      WHERE puntos_rotten='NO DATA';   
+     
+-- eliminar puntos rotten por denajo de 5 (no son puntos de rotten)
+  -- --------------------------------------------------------------
+UPDATE puntuaciones
+     SET puntos_rotten='' 
+     WHERE puntos_rotten < 5;     
  
  # SET sql_mode = "";    
 
@@ -18,7 +24,13 @@ MODIFY COLUMN puntos_rotten SMALLINT;
 ALTER TABLE puntuaciones
 MODIFY COLUMN puntos_imdb FLOAT;
 
-  ### PELICULAS 
+/* ALTER TABLE puntuaciones
+MODIFY COLUMN puntos_rotten VARCHAR(100);
+
+ALTER TABLE puntuaciones
+MODIFY COLUMN puntos_imdb VARCHAR(100);*/ 
+
+### PELICULAS 
   -- Cambiar año y mes a SMALLINT
   -- ----------------------------------
 ALTER TABLE peliculas
@@ -26,4 +38,33 @@ MODIFY COLUMN año_estreno SMALLINT;
 
 ALTER TABLE peliculas
 MODIFY COLUMN mes_estreno SMALLINT;
+
+-- Cambiar 'NO DATA' Y 'Formato min incorrecto' por string vacio
+-- ----------------------------------
+  UPDATE peliculas
+     SET duracion='' 
+     WHERE duracion='NO DATA';
+
+UPDATE peliculas
+     SET duracion='' 
+     WHERE duracion='Formato min incorrecto';   
+     
+ALTER TABLE peliculas
+MODIFY COLUMN duracion SMALLINT;   
+
+-- nombre peliculas más puntuadas
+-- ----------------------------------
+  
+
+
+select * from peliculas;
+select * from puntuaciones;
+select * from datos_interpretes;
+select * from oscars;
+select * from interpretes_peliculas;
+select * from guionistas_peliculas;
+
+
+
+
      
